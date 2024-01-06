092923b2ae93c22e5757df772aeb8c6d8703bde3
zelbassa <zelbassa@1337.student.ma>
2024-01-06 22:54:18 +0100

    MEssage

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: zelbassa <zelbassa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 21:21:54 by zelbassa          #+#    #+#             */
-/*   Updated: 2024/01/05 22:33:28 by zelbassa         ###   ########.fr       */
+/*   Updated: 2024/01/06 21:32:30 by zelbassa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,70 +30,160 @@ int	is_int(const char *str)
 	return (1);
 }
 
-char	**fill_arguments(char **av, int ac)
+void    free_stack(int **stack)
 {
-	char	**stack;
-	char	**tokens;
-	int		i;
-	int		j;
-
-	j = 0;
-	i = 0;
-	while (i < ac)
-	{
-		tokens = ft_split(av[i], ' ');
-		while (tokens[j])
-			j++;
-		free(tokens);
-		i ++;
-	}
-	stack = malloc(sizeof(char *) * (j + 1));
-	if (!stack)
-		return (NULL);
-	i = 0;
-	while (i < ac)
-	{
-		tokens = ft_split(av[i], ' ');
-		if (tokens)
-		{
-			j = 0;
-			while (tokens[j])
-			{
-				stack[j] = strdup(tokens[j]);
-				free(tokens[j]);
-				j++;
-			}
-			free(tokens);
-		}
-		i ++;
-	}
-	stack[j] = NULL;
-	return (stack);
+    free(*stack);
+    *stack = NULL;
 }
 
-int	main(int ac, char **av)
+void    fill_single_argument(char **av, int **stack)
 {
-	int		i;
-	char	**stack;
+    int     i;
+    char    **tokens;
 
-	if (ac < 2)
-	{
-		ft_printf("Error\n");
-		return (0);
-	}
-	stack = fill_arguments(av, ac);
-	i = 0;
-	while (stack[i])
-	{
-		ft_printf("%s\n", (*stack)[i]);
-		i ++;
-	}
-	i = 0;
-	while (stack[i])
-	{
-		free(stack[i]);
-		i++;
-	}
-	free(stack);
-	return (0);
+    i = 0;
+    tokens = ft_split(av[1], ' ');
+    if (tokens)
+    {
+        while (tokens[i])
+            if (is_int(tokens[i]))
+                i ++;
+            else
+                return ;
+    }
+    if (i < 2)
+        return ;
+    *stack = malloc(sizeof(int) * i);
+    i = 0;
+    while (tokens[i])
+    {
+        (*stack)[i] = ft_atoi(ft_strdup(tokens[i]));
+        i ++;
+    }
 }
+
+void    fill_multiple_arguments(char **av, int ac, int **stack)
+{
+    int     i;
+
+    i = 0;
+    *stack = malloc(sizeof(int) * (ac - 1));
+    while (i < ac)
+    {
+        if (is_int(av[i]))
+            (*stack)[i] = ft_atoi(av[i]);
+        else
+        {
+            free_stack(stack);
+            return ;
+        }
+        i ++;
+    }
+}
+
+int main(int ac, char **av)
+{
+    int i;
+    int *stack;
+
+    stack = NULL;
+    i = 0;
+    if (ac < 2)
+    {
+        ft_printf("Error\n");
+        return (0);
+    }
+    if (ac == 2)
+        fill_single_argument(av, &stack);
+    else
+        fill_multiple_arguments(av, ac, &stack);
+    if (!stack)
+        ft_printf("Error\n");
+    while (stack[i])
+    {
+        ft_printf("%d\n", stack[i]);
+        i ++;
+    }
+    i = 0;
+    free_stack(&stack);
+    return (0);
+}
+
+//int	*fill_arguments(char **av)
+//{
+//	int		i;
+//	int		*stack;
+//	char	**tokens;
+//
+//	i = 1;
+//	tokens = ft_split(av[1], ' ');
+//	if (!tokens)
+//		return (0);
+//	while(tokens[i])
+//	{
+//		if (is_int(tokens[i]))
+//			i++;
+//		else
+//		{
+//			free(tokens);
+//			return (0);
+//		}
+//	}
+//	stack = malloc(sizeof(int) * (i + 1));
+//	i = 0;
+//	while (tokens[i])
+//	{
+//		stack[i] = ft_atoi(ft_strdup(tokens[i]));
+//		i ++;
+//	}
+//    if (stack[1] == '\0')
+//        return (0);
+//	stack[i] = '\0';
+//	return (stack);
+//}
+//
+//int	*fill_list(char **av, int ac)
+//{
+//	int		i;
+//	int		*stack;
+//
+//	stack = malloc(sizeof(int) * ac);
+//	i = 0;
+//	while (i < ac)
+//	{
+//		stack[i] = ft_atoi(av[i + 1]);
+//		i ++;
+//	}
+//	stack[i] = 0;
+//	return (stack);
+//}
+//
+//int	main(int ac, char **av)
+//{
+//	int		i;
+//	int	    *stack;
+//
+//	stack = NULL;
+//	if (ac < 2)
+//	{
+//		ft_printf("Error\n");
+//		return (0);
+//	}
+//	if (ac == 2)
+//		stack = fill_arguments(av);
+//	else
+//		stack = fill_list(av, ac);
+//	if (!stack)
+//	{
+//		printf("Error\n");
+//		return (0);
+//	}
+//	i = 0;
+//	while (stack[i])
+//	{
+//		ft_printf("%d\n", stack[i]);
+//		i ++;
+//	}
+//	free(stack);
+//	return (0);
+//}
